0fbab4e0f1f50908553dad6860e4c6602e257a80
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-13 13:17:51 +0100

    app.py modified

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'sasha'
+app.secret_key = 'nourf'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
