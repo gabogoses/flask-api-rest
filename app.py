e007e76e9d9b93ef39f6690ac5d202696c867e6c
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-13 13:03:01 +0100

    files modified

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'sasha'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
     from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
