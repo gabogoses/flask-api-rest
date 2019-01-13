52fbca9ca08cce92a370fafd00842e7afd87ed48
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-13 12:51:48 +0100

    app.py modified

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-db.init_app(app)
 if __name__ == '__main__':
     from db import db
     app.run(port=5000, debug=True)
+
+db.init_app(app)
